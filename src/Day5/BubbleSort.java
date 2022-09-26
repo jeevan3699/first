276d9d927bb42fca1f2e80211f6fa45360fa932d
jeevan <jeevan3699@gmail.com>
2022-09-26 14:12:36 +0530

    Initial version

---
+++ src/Day5/BubbleSort.java
@@ -0,0 +1,34 @@
+package Day5;
+class BubbleSort {
+    void BubbleSort(int[] arr)
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n =arr.length;
+        for (int j : arr) System.out.print(j + " ");
+        System.out.println();
+    }
+
+    // Driver method to test above
+    public static void main(String[] args)
+    {
+        bubbleSort ob = new bubbleSort();
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
+
